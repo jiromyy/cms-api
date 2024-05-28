e2ac1b58cf699386f56fcde3f9978759675508d2
jiromyy <106416321+jiromyy@users.noreply.github.com>
2024-05-28 22:46:13 +0800

    added delete function

--- src/blueprints/content_manager.py
+++ src/blueprints/content_manager.py
@@ -4,6 +4,9 @@ from dateutil import tz
 from flask import jsonify, request
 from flask_smorest import Blueprint
 from flask.views import MethodView
+from azure.search.documents import SearchClient
+from azure.core.credentials import AzureKeyCredential
+from src.config.app_config_values import get_config_values
 from src.blob_manager import BlobManager
 from src.index_manager import IndexManager
 from src.auth import validate_secret_key
@@ -40,8 +43,8 @@ class ContentManager(MethodView):
         return response
     
     
-@content_manager_bp.route("/upload")
-class UploadView(MethodView):
+@content_manager_bp.route("/process/<file>")
+class ProcessView(MethodView):
     
     @content_manager_bp.arguments(HeaderDataSchema, location="headers")
     @content_manager_bp.response(200, ReturnDataSchema)
@@ -54,20 +57,35 @@ class UploadView(MethodView):
             data=header_data.user
         )
         return response
+    
+    @content_manager_bp.arguments(HeaderDataSchema, location="headers")
+    @content_manager_bp.response(200, ReturnDataSchema)
+    @validate_secret_key.validate_secret_key
+    def delete(self, header_data: HeaderData, file:str):
+        
+        app_id = request.headers.get("X-APP-ID")
+        app_config = fetch_app_config(app_id)
+        config_values = get_config_values(app_config)
 
-@content_manager_bp.route("/delete")
-class DeleteView(MethodView):
-        @content_manager_bp.arguments(HeaderDataSchema, location="headers")
-        @content_manager_bp.response(200, ReturnDataSchema)
-        @validate_secret_key.validate_secret_key
-        def delete(self, header_data: HeaderData):
+        aisearch_credentials = AzureKeyCredential(config_values.get("aisearch_key"))
+
+        index = IndexManager(config_values.get("aisearch_service_endpoint"), config_values.get("aisearch_index"), config_values.get("aisearch_key"), config_values.get("openai_api_key"), config_values.get("openai_api_version"), config_values.get("openai_endpoint"), config_values.get("blob_connection_string"), config_values.get("blob_name_preprocessing"))
+
+        index_list = index.list_index_documents(f"document_title eq '{file}'")
+        
+        search_client = SearchClient(endpoint=config_values.get("aisearch_service_endpoint"), index_name=config_values.get("aisearch_index"), credential=aisearch_credentials)
+        
+        
+        for item in index_list:
+            document_key = item.get('id')
+            res = search_client.delete_documents([{"@search.action": "delete", "id": document_key}])
             
-            response = ReturnData(
-                status=200,
-                message="Content Manager API is working",
-                data=header_data.user
-            )
-            return response
+        response = ReturnData(
+            status=200,
+            message="Content deleted successfully",
+            data=header_data.user
+        )
+        return response
         
 @content_manager_bp.route("/update")
 class UpdateView(MethodView):
@@ -92,31 +110,18 @@ class ListView(MethodView):
         
         app_id = request.headers.get("X-APP-ID")
         app_config = fetch_app_config(app_id)
-        
-        aisearch_service_endpoint = os.getenv(app_config.config_values["ai_search_config"]["aisearch_service_endpoint"])
-        aisearch_index = os.getenv(app_config.config_values["ai_search_config"]["aisearch_index"])
-        aisearch_key = os.getenv(app_config.config_values["ai_search_config"]["aisearch_key"])
-        openai_api_key = os.getenv(app_config.config_values["azure_openai_resource"]["openai_api_key"])
-        openai_api_version = os.getenv(app_config.config_values["azure_openai_resource"]["openai_api_version"])
-        openai_endpoint = os.getenv(app_config.config_values["azure_openai_resource"]["openai_endpoint"])
-        blob_connection_string = os.getenv(app_config.config_values["blob_storage_config"]["blob_connection_string"])
-        blob_name_preprocessing = os.getenv(app_config.config_values["blob_storage_config"]["blob_name_preprocessing"])
-        blob_name_ccu = os.getenv(app_config.config_values["blob_storage_config"]["blob_name_ccu"])
-        blob_name_aspen = os.getenv(app_config.config_values["blob_storage_config"]["blob_name_aspen"])
-        blob_name_rlc = os.getenv(app_config.config_values["blob_storage_config"]["blob_name_rlc"])
-        blob_name_jgsoc = os.getenv(app_config.config_values["blob_storage_config"]["blob_name_jgsoc"])
-        blob_name_rrhi = os.getenv(app_config.config_values["blob_storage_config"]["blob_name_rrhi"])
+        config_values = get_config_values(app_config)
         
         blob_name = {
-            "ccu": blob_name_ccu,
-            "aspen": blob_name_aspen,
-            "rrhi": blob_name_rrhi,
-            "rlc": blob_name_rlc,
-            "jgsoc": blob_name_jgsoc
+            "ccu": config_values.get("blob_name_ccu"),
+            "aspen": config_values.get("blob_name_aspen"),
+            "rrhi": config_values.get("blob_name_rrhi"),
+            "rlc": config_values.get("blob_name_rlc"),
+            "jgsoc": config_values.get("blob_name_jgsoc")
         }
         
-        index = IndexManager(aisearch_service_endpoint, aisearch_index, aisearch_key, openai_api_key, openai_api_version, openai_endpoint, blob_connection_string, blob_name_preprocessing)
-        blob = BlobManager(blob_connection_string)
+        index = IndexManager(config_values.get("aisearch_service_endpoint"), config_values.get("aisearch_index"), config_values.get("aisearch_key"), config_values.get("openai_api_key"), config_values.get("openai_api_version"), config_values.get("openai_endpoint"), config_values.get("blob_connection_string"), config_values.get("blob_name_preprocessing"))
+        blob = BlobManager(config_values.get("blob_connection_string"))
         
         print("Index", index)
         print("Blob", blob.blob_service_client)
@@ -124,10 +129,8 @@ class ListView(MethodView):
         index_list = index.list_index_documents(f"category eq '{function}'")
         
         files = []
-
+        item_schema = ListItemSchema()
         for item in index_list:
-            
-            item_schema = ListItemSchema()
             
             if not any(d['document_title'] == item['document_title'] for d in files):
                 del item["@search.score"]
@@ -138,24 +141,27 @@ class ListView(MethodView):
                 del item["content"]
                 del item["file_location"]
                 
-                if "generic" in applicability:
-                    if function.lower()+'/generic' in item['download_url']:
-                        blob_client = blob.blob_service_client.get_blob_client(blob_name_ccu, function.lower()+'/generic/'+item['document_title'])
-                        blob_properties = blob_client.get_blob_properties()
-                        item["file_size"] = f"{round(int(blob_properties.size)/1024/1024,2)}MB"
-                        item["created_date"] = blob_properties.creation_time.astimezone(tz.tzlocal()).strftime("%m-%d-%Y %I:%M %p")
-                        item["modified_date"] = blob_properties.last_modified.astimezone(tz.tzlocal()).strftime("%m-%d-%Y %I:%M %p")
-                        files.append(item_schema.dump(item))
+                if function != "HR":
+                    files.append(item_schema.dump(item))
                 else:
-                    if blob_name[applicability.lower()]+'/'+function.lower()+'/specific' in item['download_url']:
-                        blob_client = blob.blob_service_client.get_blob_client(blob_name[applicability.lower()], function.lower()+'/specific/'+item['document_title'])
-                        blob_properties = blob_client.get_blob_properties()
-                        item["file_size"] = f"{round(int(blob_properties.size)/1024/1024,2)}MB"
-                        item["created_date"] = blob_properties.creation_time.astimezone(tz.tzlocal()).strftime("%m-%d-%Y %I:%M %p")
-                        item["modified_date"] = blob_properties.last_modified.astimezone(tz.tzlocal()).strftime("%m-%d-%Y %I:%M %p")
-                        files.append(item_schema.dump(item))
+                
+                    if "generic" in applicability:
+                        if function.lower()+'/generic' in item['download_url']:
+                            blob_client = blob.blob_service_client.get_blob_client(config_values.get("blob_name_ccu"), function.lower()+'/generic/'+item['document_title'])
+                            blob_properties = blob_client.get_blob_properties()
+                            item["file_size"] = f"{round(int(blob_properties.size)/1024/1024,2)}MB"
+                            item["created_date"] = blob_properties.creation_time.astimezone(tz.tzlocal()).strftime("%m-%d-%Y %I:%M %p")
+                            item["modified_date"] = blob_properties.last_modified.astimezone(tz.tzlocal()).strftime("%m-%d-%Y %I:%M %p")
+                            files.append(item_schema.dump(item))
+                    else:
+                        if blob_name[applicability.lower()]+'/'+function.lower()+'/specific' in item['download_url']:
+                            blob_client = blob.blob_service_client.get_blob_client(blob_name[applicability.lower()], function.lower()+'/specific/'+item['document_title'])
+                            blob_properties = blob_client.get_blob_properties()
+                            item["file_size"] = f"{round(int(blob_properties.size)/1024/1024,2)}MB"
+                            item["created_date"] = blob_properties.creation_time.astimezone(tz.tzlocal()).strftime("%m-%d-%Y %I:%M %p")
+                            item["modified_date"] = blob_properties.last_modified.astimezone(tz.tzlocal()).strftime("%m-%d-%Y %I:%M %p")
+                            files.append(item_schema.dump(item))
                         
-
         response = ListReturnData(
             status=200,
             message="Successfuly retrieved list of files",
--- src/config/app_config_values.py
+++ src/config/app_config_values.py
@@ -1,5 +1,24 @@
 from pydantic import BaseModel
+import os
 
 # TODO: move this to the config folder/directory
 class AppConfigValues(BaseModel):
     config_values: dict
+    
+def get_config_values(app_config):
+    config_values = {
+        "aisearch_service_endpoint": os.getenv(app_config.config_values["ai_search_config"]["aisearch_service_endpoint"]),
+        "aisearch_index": os.getenv(app_config.config_values["ai_search_config"]["aisearch_index"]),
+        "aisearch_key": os.getenv(app_config.config_values["ai_search_config"]["aisearch_key"]),
+        "openai_api_key": os.getenv(app_config.config_values["azure_openai_resource"]["openai_api_key"]),
+        "openai_api_version": os.getenv(app_config.config_values["azure_openai_resource"]["openai_api_version"]),
+        "openai_endpoint": os.getenv(app_config.config_values["azure_openai_resource"]["openai_endpoint"]),
+        "blob_connection_string": os.getenv(app_config.config_values["blob_storage_config"]["blob_connection_string"]),
+        "blob_name_preprocessing": os.getenv(app_config.config_values["blob_storage_config"]["blob_name_preprocessing"]),
+        "blob_name_ccu": os.getenv(app_config.config_values["blob_storage_config"]["blob_name_ccu"]),
+        "blob_name_aspen": os.getenv(app_config.config_values["blob_storage_config"]["blob_name_aspen"]),
+        "blob_name_rlc": os.getenv(app_config.config_values["blob_storage_config"]["blob_name_rlc"]),
+        "blob_name_jgsoc": os.getenv(app_config.config_values["blob_storage_config"]["blob_name_jgsoc"]),
+        "blob_name_rrhi": os.getenv(app_config.config_values["blob_storage_config"]["blob_name_rrhi"]),
+    }
+    return config_values
